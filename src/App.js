22f21950b7be12d590f9e009371ee109e284cc0a
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-11 21:37:45 -0500

    GOT THE THING TO WORK! kinda good enough.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import Toolbar from './components/Toolbar/Toolbar.jsx';
 import SideDrawer from './components/SideDrawer/SideDrawer.jsx'
 import Backdrop from './components/Backdrop/Backdrop.jsx';
 import Footer from './containers/footer/Footer.js'
-// import { CSSTransition } from "react-transition-group";
-// import data from './assests/Pics/data'
-// import Home from "./assests/Pics/homes";
+import { CSSTransition } from "react-transition-group";
+import data from './assests/Pics/data';
+import Home from "./assests/Pics/homes";
 
  
 const mapStateToProps = state => ({
@@ -18,39 +18,39 @@ const mapStateToProps = state => ({
  const mapDispatchToProps = dispatch => ({
   simpleAction: () => dispatch(simpleAction())
  });
-/*
-simpleAction = (event) => {
-  this.props.simpleAction();
-};*/
+
+// simpleAction = (event) => {
+//   this.props.simpleAction();
+// };
 class App extends Component {
 
-  // constructor(props){
-  //   super(props);
-  //   this.state = {
-  //     appearHome: true,
-  //     property: data.properties[0]
-  //   }
-  // }
+  constructor(props){
+    super(props);
+    this.state = {
+      appearHome: true,
+      property: data.properties[0]
+    }
+  }
 
-  // toggleAppear = () => {
-  //   this.setState({
-  //     appearHome: !this.state.appearHome
-  //   })
-  // }
+  toggleAppear = () => {
+    this.setState({
+      appearHome: !this.state.appearHome
+    })
+  }
 
-  // nextProperty = () => {
-  //   const newIndex = this.state.property.index+1;
-  //   this.setState({
-  //     property: data.properties[newIndex]
-  //   })
-  // }
+  nextProperty = () => {
+    const newIndex = this.state.property.index+1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
 
-  // prevProperty = () => {
-  //   const newIndex = this.state.property.index-1;
-  //   this.setState({
-  //     property: data.properties[newIndex]
-  //   })
-  // }
+  prevProperty = () => {
+    const newIndex = this.state.property.index-1;
+    this.setState({
+      property: data.properties[newIndex]
+    })
+  }
 
 
   state = {
@@ -68,7 +68,7 @@ backdropClickHandler=()=>{
 
 
   render() {
-    // const {appearHome, property} = this.state;
+    const {appearHome, property} = this.state;
     let backdrop;
 
     if (this.state.sideDrawerOpen){
@@ -79,14 +79,14 @@ backdropClickHandler=()=>{
     return (
     <div className="App">
       <header  className="heropanel">
-        {/* <button onClick={() => this.toggleAppear()}>Appear: {`${appearHome}`}</button>
-        <button onClick={() => this.nextProperty()} disabled={property.index === data.properties.length-1}>Next</button>
-        <button onClick={() => this.prevProperty()} disabled={property.index === 0}>Prev</button>
+         {/* <button onClick={() => this.toggleAppear()}>Appear: {`${appearHome}`}</button> */}
+        {/* <button onClick={() => this.nextProperty()} disabled={property.index === data.properties.length-1}>Next</button> */}
+        {/* <button onClick={() => this.prevProperty()} disabled={property.index === 0}>Prev</button> */}
         <CSSTransition
         in={appearHome}
         appear={true}
-        timeout={1000}
-        classNames="fade"> */}
+        timeout={5000}
+        classNames="fade">
 
 
       <div className="v-header container">
@@ -98,11 +98,11 @@ backdropClickHandler=()=>{
         </div> 
         {backdrop}
       </div>
-        {/* <Home property={property} />
-        </CSSTransition> */}
+         {/* <Home property={property} /> */}
+        </CSSTransition>
 
         <Toolbar drawerClickHandler={this.drawerToggleClickHandler}/>
-        <SideDrawer show={this.state.sideDrawerOpen}/>
+        <SideDrawer show={this.state.sideDrawerOpen}/> 
 
   
     </header>
@@ -122,6 +122,17 @@ backdropClickHandler=()=>{
    </div>
    </div>
    </div>
+   <button onClick={() => this.toggleAppear()}>Appear: {`${appearHome}`}</button>
+   <button onClick={() => this.nextProperty()} disabled={property.index === data.properties.length-1}>Next</button>
+   <button onClick={() => this.prevProperty()} disabled={property.index === 0}>Prev</button>
+        <CSSTransition
+        in={appearHome}
+        appear={true}
+        timeout={3000}
+        classNames="fade"
+        >
+        <Home property={property} />
+        </CSSTransition>
    <Footer />
    </div>
   );
