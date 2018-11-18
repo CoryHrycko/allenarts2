d530b2cf50a7c9ee3d07b8f5c30ff0cbe999b570
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-17 22:14:27 -0500

    Making a better entrance and breaking down all the elements into their own "pages"

--- src/components/buttons/entry.js
+++ src/components/buttons/entry.js
@@ -1,27 +1,39 @@
-const Greeting = React.createClass({
-    render:()=>{
-        let classMale = "";
-        let classFemale = "";
+// const Greeting = React.createClass({
+//     render:()=>{
+//         let classMale = "";
+//         let classFemale = "";
 
-        if(this.props.gender === "male"){
-            classFemale = "displayNone";
-        }
-        else{
-            classMale = "displayNone";
-        }
-        return (
-            <div>
-                <div>Greeting</div>
+//         if(this.props.gender === "male"){
+//             classFemale = "displayNone";
+//         }
+//         else{
+//             classMale = "displayNone";
+//         }
+//         return (
+//             <div>
+//                 <div>Greeting</div>
 
-                <div className={classFemale}>Hello Mrs. Jones
-                    <div className="appLogoContainer">
-                        <button className="entering" onclick="entering()">Press to enter
-                            <img src={Capture} className="App-logo" alt="logo" />
-                        </button>
-                    </div>
-                </div>
-                <div className={classMale}>Hello Mr. Jones</div>
-            </div>
-        );
-    }
-});
+//                 <div className={classFemale}>Hello Mrs. Jones
+//                     <div className="appLogoContainer">
+//                         <button className="entering" onclick="entering()">Press to enter
+//                             <img src={Capture} className="App-logo" alt="logo" />
+//                         </button>
+//                     </div>
+//                 </div>
+//                 <div className={classMale}>Hello Mr. Jones</div>
+//             </div>
+//         );
+//     }
+// });
+
+
+constructor(){
+    super();
+
+    this.state={
+        hideToolTip:false
+    };
+};
+// handleChange(e) {
+//     this.setState({hideToolTip: true});
+// };
