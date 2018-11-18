07790b8673f949a8293c1d118c7a4ab53f964381
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-18 00:23:49 -0500

    Making a better entrance and breaking down all the elements into their own "pages"

--- src/App.js
+++ src/App.js
@@ -129,7 +129,11 @@ backdropClickHandler=()=>{
         </div>
     </div> 
 {/* ------------------------------------main contact and calendir*/}
-    <div className="mainBg" id="c3">
+    <div className="mainBg2" id="c3">
+    </div>
+    
+    
+    {/* <div className="mainBg" id="c3">
      <div className="mainShape">
    <div className="mainShapeTitle">
      <h3> Recent Events!(Api End point get parsed here.)</h3>
@@ -144,9 +148,9 @@ backdropClickHandler=()=>{
         <Home property={property} />
 
         </CSSTransition>
-   </div>
+   </div> */}
    {/* ----------------------------------------contact and scheduling */}
-   <div className="mainShapeTitle2" id="c4">
+   {/* <div className="mainShapeTitle2" id="c4">
      <h3> PAst events go in here</h3>
    </div>
    <div className="mainShapeTitle3">
@@ -214,26 +218,26 @@ function test_input($data) {
 ?>*/}
 
 
-     <h3>Book!(PHP is inserted here to set up an email alert to the manager and schedule the event in the database which then takes it and compiles it onto the calendir</h3>
-    <div className="bookInput">
-    <form method="post" action="*">{/*<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  */}
-      <input className="bookName" placeholder="Name of Organization"></input>{/*   Name: <input type="text" name="name" value="<?php echo $name;?>">
-  <span class="error">* <?php echo $nameErr;?></span>
-  <br><br> */}
-      <input className="bookDate" datatype="date" placeholder="MM/DD/YYYY"></input>{/*   E-mail: <input type="text" name="date" value="<?php echo $date;?>">
-  <span class="error">* <?php echo $dateErr;?></span>
-  <br><br> */}
-      <input className="bookEmail" placeholder="example@example.coms"></input>{/*   E-mail: <input type="text" name="email" value="<?php echo $email;?>">
-  <span class="error">* <?php echo $emailErr;?></span>
-  <br><br> */}
-      <textarea className="bookDescribes" placeholder="Please describe the event"></textarea>{/*   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
-  <br><br> */}
-      <input type="submit"  value="Submit your request" />{/**/}
+     {/* <h3>Book!(PHP is inserted here to set up an email alert to the manager and schedule the event in the database which then takes it and compiles it onto the calendir</h3> */}
+    {/* <div className="bookInput"> */}
+    {/* <form method="post" action="*"><form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  */}
+      {/* <input className="bookName" placeholder="Name of Organization"></input>{/*   Name: <input type="text" name="name" value="<?php echo $name;?>"> */}
+  {/* <span class="error">* <?php echo $nameErr;?></span> */}
+  {/* <br><br> */}
+      {/* <input className="bookDate" datatype="date" placeholder="MM/DD/YYYY"></input>{/*   E-mail: <input type="text" name="date" value="<?php echo $date;?>"> */}
+  {/* <span class="error">* <?php echo $dateErr;?></span> */}
+  {/* <br><br> */} 
+      {/* <input className="bookEmail" placeholder="example@example.coms"></input>{/*   E-mail: <input type="text" name="email" value="<?php echo $email;?>"> */}
+  {/* <span class="error">* <?php echo $emailErr;?></span> */}
+  {/* <br><br> */} 
+      {/* <textarea className="bookDescribes" placeholder="Please describe the event"></textarea>{/*   Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea> */}
+  {/* <br><br> */} 
+      {/* <input type="submit"  value="Submit your request" />{/*
     </form>
     </div>
    </div>
-   </div>
-   </div>
+   </div> 
+   </div>*/} 
    <Footer />
    </div>
   );
