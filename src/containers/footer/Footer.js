7d01eae87c3635c549dccc430006c6931128f056
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-26 23:03:45 -0500

    Updating the footer.

--- src/containers/footer/Footer.js
+++ src/containers/footer/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Date from '../../assests/Date'
 
 
-const footer2 = props => (
+const footer2 = props => ( 
 <div className="footer">Copyright © &nbsp;<Date /> &nbsp;Allen Arts Music Group. All rights reserved.
         
     </div>
